44a65cf1db6aecf70b6e80335a3e04d839b9e318
soumen bera <soumenbera211@gmail.com>
2026-07-29 19:54:42 +0530

    payment system added

--- index.js
+++ index.js
@@ -1,45 +1,181 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const port = process.env.PORT || 3000
-// import { MongoClient } from 'mongodb';
-const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const { MongoClient, ObjectId } = require("mongodb");
+
+const crypto = require("crypto");
+
+function generateTrackingId() {
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+
+  const randomPart = crypto.randomBytes(4).toString("hex").toUpperCase();
+
+  return `TRK-${date}-${randomPart}`;
+}
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-const client = new MongoClient(`mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.kbxs8tk.mongodb.net/?appName=Cluster0`);
+const client = new MongoClient(
+  `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.kbxs8tk.mongodb.net/?appName=Cluster0`,
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello World recap full stack project!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World recap full stack project!");
+});
 
- async function connectToMongoDB() {
+async function connectToMongoDB() {
   try {
     await client.connect();
 
-    const db = client.db('recapZapShiftDB');
-    const parcelsCollection = db.collection('parcels')
+    const db = client.db("recapZapShiftDB");
+    const parcelsCollection = db.collection("parcels");
+    const paymentCollection = db.collection("payment");
 
     // parcels api
-    app.post('/parcels', async(req, res) => {
-        const parcel = req.body;
-        const result = await parcelsCollection.insertOne(parcel);
-        res.send(result)
-    })
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      // created time
+      parcel.createdAt = new Date();
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
 
-    app.get('/parcels', async(req, res) => {
-        const query = {};
-        const {email} = req.query;
-        if(email){
-            query.senderEmail = email;
+    // payment karar samoy single parcel er data paoyar jonno get api
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      if (email) {
+        query.senderEmail = email;
+      }
+      const option = { sort: { createdAt: -1 } };
+      const cursor = parcelsCollection.find(query, option);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // stripe er jonno payment related api
+    // same page theke karar jonno api
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              unit_amount: amount,
+              product_data: {
+                name: `Please Pay For : ${paymentInfo.parcelName}`,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+          parcelName: paymentInfo.parcelName,
+        },
+        customer_email: paymentInfo.senderEmail,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+      res.send({ url: session.url });
+    });
+
+    // success houyar por session id ta niye kaj korar jonno patch api
+    app.patch("/payment-success", async (req, res) => {
+      const sessionId = req.query.session_id;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      console.log("session retrieve is", session);
+
+      const trackingId = generateTrackingId();
+
+      if (session.payment_status === "paid") {
+        const id = session.metadata.parcelId;
+        const query = { _id: new ObjectId(id) };
+        const update = {
+          $set: {
+            paymentStatus: "paid",
+            trackingId: trackingId,
+          },
+        };
+        const result = await parcelsCollection.updateOne(query, update);
+
+        const payment = {
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          customerEmail: session.customer_email,
+          parcelId: session.metadata.parcelId,
+          parcelName: session.metadata.parcelName,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paidAt: new Date(),
+        };
+
+        if (session.payment_status === "paid") {
+          const resultPayment = await paymentCollection.insertOne(payment);
+          res.send({
+            success: true,
+            modifyParcel: result,
+            paymentInfo: resultPayment,
+            trackingId: trackingId,
+            transactionId: session.payment_intent,
+          });
         }
-        const cursor = parcelsCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+      }
+      res.send({ success: false });
+    });
+
+    // old onno page theke korar jonno api
+    // app.post("/create-checkout-session", async (req, res) => {
+    //   const paymentInfo = req.body;
+    //   const amount = parseInt(paymentInfo.cost) * 100;
+
+    //   const session = await stripe.checkout.sessions.create({
+    //     line_items: [
+    //       {
+    //         price_data: {
+    //           currency: "usd",
+    //           unit_amount: amount,
+    //           product_data: {
+    //             name: paymentInfo.parcelName,
+    //           },
+    //         },
+
+    //         quantity: 1,
+    //       },
+    //     ],
+    //     customer_email: paymentInfo.senderEmail,
+    //     mode: "payment",
+    //     metadata: {
+    //       parcelId: paymentInfo.parcelId,
+    //     },
+    //     success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+    //     cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+    //   });
+    //   console.log(session);
+    //   res.send({ url: session.url });
+    // });
 
     console.log("You successfully connected to MongoDB!");
     return client;
@@ -50,5 +186,5 @@ app.get('/', (req, res) => {
 connectToMongoDB();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
